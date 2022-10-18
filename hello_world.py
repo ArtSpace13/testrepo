d405299b366dd8a39fea6f7dde54b73b0fce31a7
ArtSpase13 <114692709+ArtSpace13@users.noreply.github.com>
2022-10-18 16:34:24 -0700

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,88 @@
+print ("Art Space")
+
+message = "art space"
+print(message.title())
+print (message.upper())
+
+
+first_name = "art"
+last_name = "space"
+print(first_name.upper())
+
+full_name = f"Hello, {first_name.title()} {last_name.title()}"
+print(full_name)
+
+full_name = f"Hello {first_name} {last_name}"
+print (full_name.title())
+
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}"
+print(message)
+
+
+print("Language: \tPython")
+print("Languages:\n\tPython\n\tC")
+
+
+name = "Art"
+age = "36"
+message = f"Hello, {name.title()}. \nYou are {age} now"
+print(message)
+
+
+bicycles = ["car", "car1", "car2"]
+print (bicycles[0].upper())
+print (bicycles[-2])
+print (f"My car is {bicycles[2].title()}")
+message = f"My car is {bicycles[2].title()}"
+print (message)
+
+
+names = ["Ben", "Alex", "Art"]
+print (names[0].title())
+print (names[1].title())
+print (names[2].title())
+message =f"My friends: \t\n{names[0].title()} \t\nand \t\n{names[1].title()} \t\nand \t\n{names[2].title()}"
+print(message)
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = "ducati"
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append ("honda")
+motorcycles.append ("suzuki")
+motorcycles.append ("yamaha")
+print(motorcycles)
+motorcycles.insert(3,"ducati")
+print(motorcycles)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
